a38e8b3272a5c23037c6e9ea27679c497b5c99af
Folitar, Sergey <s.folitar@itransition.com>
2016-07-20 16:55:41 +0300

    product entity created

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -17,6 +17,7 @@ class AppKernel extends Kernel
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new AppBundle\AppBundle(),
             new \Ddeboer\DataImportBundle\DdeboerDataImportBundle(),
+            new ImportBundle\ImportBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'), true)) {
--- src/ImportBundle/ImportBundle.php
+++ src/ImportBundle/ImportBundle.php
@@ -1,9 +1,9 @@
 <?php
 
-namespace AppBundle;
+namespace ImportBundle;
 
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
-class AppBundle extends Bundle
+class ImportBundle extends Bundle
 {
 }
