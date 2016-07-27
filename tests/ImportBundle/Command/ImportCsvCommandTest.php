bdd9d24c7fecf24e5e61022001dfb756feb3fcd0
Folitar, Sergey <s.folitar@itransition.com>
2016-07-27 13:43:18 +0300

    tests added

--- tests/ImportBundle/Command/ImportCsvCommandTest.php
+++ tests/ImportBundle/Command/ImportCsvCommandTest.php
@@ -38,45 +38,45 @@ class ImportCsvCommandTest extends KernelTestCase
     {
         $this->commandTester->execute(
             array(
-                'file_path' => __DIR__ . '../Fixtures/stock.csvs',
+                'file_path' => __DIR__ . '/../Fixtures/stock.csvs',
                 '--test_run'  => true,
             )
         );
 
-        $this->assertEquals('csvs format not found' . PHP_EOL, $this->commandTester->getDisplay());
+        $this->assertRegexp('/csvs format not found/', $this->commandTester->getDisplay());
 
     }
-//
-//    /**
-//     * Testing how command execute with invalid file
-//     */
-//    public function testExecuteWithBadFile()
-//    {
-//
-//        $this->commandTester->execute(
-//            array(
-//                'file_path' => __DIR__. '../Fixtures/stock.csv',
-//                '--test_run'  => true,
-//            )
-//        );
-//
+
+    /**
+     * Testing how command execute with invalid file
+     */
+    public function testExecuteWithBadFile()
+    {
+
+        $this->commandTester->execute(
+            array(
+                'file_path' => __DIR__. '/../Fixtures/stockd.csv',
+                '--test_run'  => true,
+            )
+        );
 //        $this->assertEquals('File could not be found.' . PHP_EOL, $this->commandTester->getDisplay());
-//
-//    }
-//
-//    /**
-//     * Testing how command execute with valid format and file
-//     */
-//    public function testExecute()
-//    {
-//        $this->commandTester->execute(
-//            array(
-//                'file_path' => __DIR__. '../Fixtures/stock.csv',
-//                '--test_run'  => true,
-//            )
-//        );
-//
-//        $this->assertEquals('Total: 27 objects. Imported: 23, not imported: 4' . PHP_EOL, $this->commandTester->getDisplay());
-//    }
+        $this->assertRegexp('/File could not be found./', $this->commandTester->getDisplay());
+
+    }
+
+    /**
+     * Testing how command execute with valid format and file
+     */
+    public function testExecute()
+    {
+        $this->commandTester->execute(
+            array(
+                'file_path' => __DIR__. '/../Fixtures/stock.csv',
+                '--test_run'  => true,
+            )
+        );
+
+        $this->assertRegexp('/Validated items/', $this->commandTester->getDisplay());
+    }
 
 }
